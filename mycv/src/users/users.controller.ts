115f3a9b926ffe7b696a7e0b2168d60eb7fde708
Buzzaw91 <aleksi.ylonen2@gmail.com>
2022-02-07 17:28:27 +0200

    add AuthGuard

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -8,7 +8,8 @@ import {
     Query,
     Delete,
     NotFoundException,
-    Session
+    Session,
+    UseGuards
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -18,6 +19,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Serialize(UserDto)
 @Controller('auth')
@@ -33,6 +35,7 @@ export class UsersController {
     // }
 
     @Get('/whoami')
+    @UseGuards(AuthGuard)
     whoAmI(@CurrentUser() user: User) {
         return user;
     }
